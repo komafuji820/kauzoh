4a1d4a018213156abd5ed933e8f117e5826621f5
komafuji820 <tfukae25@gmail.com>
2023-12-24 18:26:56 +0900

    バリデーションの設定

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,15 +1,14 @@
 class Category < ActiveHash::Base
   self.data = [
-    { id: 1, name: '---' },
-    { id: 2, name: '経済' },
-    { id: 3, name: '政治' },
-    { id: 4, name: '地域' },
-    { id: 5, name: '国際' },
-    { id: 6, name: 'IT' },
-    { id: 7, name: 'エンタメ' },
-    { id: 8, name: 'スポーツ' },
-    { id: 9, name: 'グルメ' },
-    { id: 10, name: 'その他' }
+    { id: 1, name: '食品・飲料' },
+    { id: 2, name: 'キッチン用品' },
+    { id: 3, name: 'トイレ・バス用品' },
+    { id: 4, name: '日用品' },
+    { id: 5, name: '医薬品' },
+    { id: 6, name: '衣料品' },
+    { id: 7, name: 'ベビー・介護用品' },
+    { id: 8, name: 'ペット用品' },
+    { id: 9, name: 'その他' }
   ]
 
   include ActiveHash::Associations
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,7 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions 
   belongs_to :category
+
+  validates :category_id, numericality: { only_integer: true, greater_than_or_equal_to: 1,less_than_or_equal_to: 9 }
   
 end
