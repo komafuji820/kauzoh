8c7f932ec8f7c5f0524b4a6718824151f0c52400
komafuji820 <tfukae25@gmail.com>
2023-12-24 17:03:21 +0900

    単体テストコードの実装

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @group = FactoryBot.build(:group)
+  end
+
+  describe "グループ新規作成" do
+    context "新規作成できるとき" do
+      it "nameを正しく入力すれば登録できる" do
+        expect(@group).to be_valid
+      end
+    end
+
+    context "新規登録できないとき" do
+      it "nameが空では登録できない" do
+        @group.name = ""
+        @group.valid?
+        expect(@group.errors.full_messages).to include("Name can't be blank")
+      end
+    end
+  end
 end
