c27bd72367020d95397685338a009ed17dacf82f
komafuji820 <tfukae25@gmail.com>
2023-12-30 09:27:04 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :group
   has_many :orders, dependent: :destroy
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions 
   belongs_to :category
-
-  has_one_attached :image
 
   validates :category_id, numericality: { only_integer: true, greater_than_or_equal_to: 1,less_than_or_equal_to: 9 }
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,8 @@ class Order < ApplicationRecord
   belongs_to :priority
   belongs_to :category
   
+  validates :priority_id, numericality: { only_integer: true, greater_than_or_equal_to: 1,less_than_or_equal_to: 4 }
+
+  validates :category_id, numericality: { only_integer: true, greater_than_or_equal_to: 1,less_than_or_equal_to: 9 }
+  
 end
