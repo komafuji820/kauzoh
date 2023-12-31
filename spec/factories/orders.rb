529c7dcb32049d3ed5ece25b9d462860a387c457
komafuji820 <tfukae25@gmail.com>
2023-12-31 10:43:46 +0900

    orderモデルの単体テストコード実装

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :order do
-    
+    memo {Faker::Lorem.word}
+    category_id {Faker::Number.between(from: 1, to: 9)}
+    priority_id {Faker::Number.between(from: 1, to: 4)}
+    association :group
+    association :item
+
   end
 end
