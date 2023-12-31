d8321dbb60410ce45abad11cef3f371ed14b43b0
komafuji820 <tfukae25@gmail.com>
2023-12-31 11:11:51 +0900

    purchaseモデルの単体テストコード実装

--- spec/factories/purchases.rb
+++ spec/factories/purchases.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :purchase do
-    
+    memo {Faker::Lorem.word}
+    category_id {Faker::Number.between(from: 1, to: 9)}
+    priority_id {Faker::Number.between(from: 1, to: 4)}
+    association :group
+
+    after(:build) do |purchase|
+      purchase.image.attach(io: File.open('public/images/test.jpg'), filename: 'test.jpg')
+    end
   end
 end
