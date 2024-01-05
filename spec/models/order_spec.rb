00ba9eb3bc592e44c8d7acfcc2dc863646c8277d
komafuji820 <tfukae25@gmail.com>
2024-01-05 23:53:19 +0900

    orderモデルのテストコードを修正

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -7,41 +7,17 @@ RSpec.describe Order, type: :model do
 
   describe "お買い物リスト新規登録（在庫リストから）" do
     context "新規登録できるとき" do
-      it "category, priority, memoを正しく入力すれば登録できる" do
+      it "priority, memoを正しく入力すれば登録できる" do
         expect(@order).to be_valid
       end
 
-      it "memoが空でも、category, priorityがあれば登録できる" do
+      it "memoが空でも、priorityがあれば登録できる" do
         @order.memo = ""
         expect(@order).to be_valid
       end
     end
 
     context "新規登録できないとき" do
-      it "categoryが空では登録できない" do
-        @order.category_id = ""
-        @order.valid?
-        expect(@order.errors.full_messages).to include("Category is not a number")
-      end
-
-      it "categoryが1~9以外（10以上）では登録できない" do
-        @order.category_id = Faker::Number.number(digits: 2)
-        @order.valid?
-        expect(@order.errors.full_messages).to include("Category must be less than or equal to 9")
-      end
-
-      it "categoryが1~9以外（0）では登録できない" do
-        @order.category_id = "0"
-        @order.valid?
-        expect(@order.errors.full_messages).to include("Category must be greater than or equal to 1")
-      end
-
-      it "categoryが整数値以外では登録できない" do
-        @order.category_id = Faker::Number.decimal(l_digits: 1)
-        @order.valid?
-        expect(@order.errors.full_messages).to include("Category must be an integer")
-      end
-
       it "priorityが空では登録できない" do
         @order.priority_id = ""
         @order.valid?
